8e44e6bce8937696081e3b4573936bca2a331566
agarciad <antonio.garcia.diaz@ulb.ac.be>
2016-12-09 14:48:07 +0100

    Legere modification de charlie

--- src/charlie.py
+++ src/charlie.py
@@ -1,4 +1,5 @@
 import sys
+from lib.client import *
 
 if len(sys.argv) < 2:
     print('Error: missing step number')
@@ -8,8 +9,11 @@ step = int(sys.argv[1])
 if step == 1:
     """
     Step 1
-    Creation of the client Charlie that will download chunks of a file from 2 peers: Alice and Bob.
+    Creation of client Charlie (downloads chunks of a file from 2 peers: Alice and Bob).
     """
+
+    Charlie('alice')
+
 
 elif step == 2:
     """
@@ -23,3 +27,10 @@ elif step == 3:
 else:
     print('Error: invalid step number')
     sys.exit(1)
+
+
+class Charlie(Client):
+
+    def __init__(self, user):
+        print(self.ip_address)
+        print(port_number)
